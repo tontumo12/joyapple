ec0d1d21644e0ca84bc7c74b303ff21d231dfd12
tontumo12 <tontumo12@gamil.com>
2019-05-10 16:35:49 +0700

    assets

---
+++ assets/js/chart-data.js
@@ -0,0 +1,42 @@
+
+	
+	var lineChartData = {
+			labels : ["Tháng 1","Tháng 2","Tháng 3","Tháng 4","Tháng 5","Tháng 6","Tháng 7","tháng 8","tháng 9","tháng 10","tháng 11","tháng 12"],
+			datasets : [
+			
+				{
+					label: "My Second dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [600,700,900,2000,4000,800,300,900,650,1230,420,762]
+				}
+			]
+
+		}
+		
+
+
+			
+
+
+window.onload = function(){
+	var chart1 = document.getElementById("line-chart").getContext("2d");
+	window.myLine = new Chart(chart1).Line(lineChartData, {
+		responsive: true
+	});
+	var chart2 = document.getElementById("bar-chart").getContext("2d");
+	window.myBar = new Chart(chart2).Bar(barChartData, {
+		responsive : true
+	});
+	var chart3 = document.getElementById("doughnut-chart").getContext("2d");
+	window.myDoughnut = new Chart(chart3).Doughnut(doughnutData, {responsive : true
+	});
+	var chart4 = document.getElementById("pie-chart").getContext("2d");
+	window.myPie = new Chart(chart4).Pie(pieData, {responsive : true
+	});
+	
+};
